91b60f07ffc7d4fcfca846f73467f68a7d44791f
exalcams <exalca.plant1@gmail.com>
2021-10-07 19:03:21 +0530

    Customer - Invoice payment

--- src/app/allModules/customer/payment-history-dialog/payment-history-dialog.component.ts
+++ src/app/allModules/customer/payment-history-dialog/payment-history-dialog.component.ts
@@ -9,29 +9,28 @@ import { DialogData } from 'app/notifications/notification-dialog/dialog-data';
   styleUrls: ['./payment-history-dialog.component.scss']
 })
 export class PaymentHistoryDialogComponent implements OnInit {
- 
+
   constructor(public matDialogRef: MatDialogRef<PaymentHistoryDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: BPCPayRecord[]) { }
-    PayDataSource: MatTableDataSource<BPCPayRecord>;
+  PayDataSource: MatTableDataSource<BPCPayRecord>;
   @ViewChild(MatPaginator) PayPaginator: MatPaginator;
   PayDisplayedColumns: string[] = [
-    'InvoiceNumber',
+    // 'InvoiceNumber',
+    'PaidAmount',
+    'ReferenceNumber',
     'Paymentdate',
-    'PaidAmount'
-   
-   
   ];
- 
-ngOnInit() {
-  if(this.data.length){
-    this.PayDataSource=new MatTableDataSource(this.data);
-    this.PayDataSource.paginator=this.PayPaginator;
+
+  ngOnInit(): void {
+    if (this.data.length) {
+      this.PayDataSource = new MatTableDataSource(this.data);
+      this.PayDataSource.paginator = this.PayPaginator;
+    }
+
   }
-  
-}
-CloseClicked(): void {
-  // console.log('Called');
-  this.matDialogRef.close();
-}
+  CloseClicked(): void {
+    // console.log('Called');
+    this.matDialogRef.close();
+  }
 
 }
